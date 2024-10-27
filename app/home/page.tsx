c4a1d4d5405caf07808bdb74c42e273c03905b6a
Alex Kumar <alexkumar520@gmail.com>
2024-10-26 23:00:15 -0700

    Initial commit. Added a basic login, middleware, and logout feature.

---
+++ app/home/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import { SignOut } from "@/components/sign-out";
+import { auth } from "@/auth"
+
+export default async function Page() {
+
+  const session = await auth();
+
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1>Welcome to boga rank, {session?.user?.email}! Thanks for coming!</h1>
+      <SignOut></SignOut>
+    </div>
+  );
+}
